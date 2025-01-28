f1bc47de804791a32ec10590db85cb32c5963f65
TheWiseLegend <B09220027@student.unimy.edu.my>
2025-01-28 16:58:25 +0800

    UseState Practised

---
+++ 307-useState-hook-practice/src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+
+
+
